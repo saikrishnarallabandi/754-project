60aa174eb2dd829b3372e785464c3cdea6dea762
Sai Krishna <srallaba@cs.cmu.edu>
2018-03-02 15:52:52 -0500

    Added map

--- baselines/01_basic_distance/DialogAgent_geocoder.py
+++ baselines/01_basic_distance/DialogAgent_geocoder.py
@@ -1,38 +1,59 @@
 import datetime
 import googlemaps
-import sys, os
+import sys
+import geocoder
+from geojsonio import display
+from shapely.geometry import Point
+
+from geopandas import GeoDataFrame
+import pandas as pd 
+import geocoder 
+import googlemaps
+from shapely.geometry import Point
+from geojsonio import display
+
+import gmplot
 
 class DialogAgent(object):
 
-    def __init__(self):
+   def __init__(self):
        date = str(datetime.datetime.now()).split()[0]
        time = str(datetime.datetime.now()).split()[1]
        self.log_file_name = 'logs/log_' + date + '_' + time 
        self.gmaps = googlemaps.Client(key='AIzaSyCMU3s5rzipRA4lXhGT8aiBaXlPJemQLF4')
 
-    def speak(self, content):
-       cmd = 'flite -t ' + content
-       os.system(cmd)      
-       return cmd
-
-    def print_logfilename(self):     
+   def print_logfilename(self):     
        print self.log_file_name
 
-    def start_chat(self):
+   def get_gdf(self):
+        crs = {'init': 'epsg:4326'}
+        return(GeoDataFrame(self.get_names(), crs=crs, geometry=self.get_geo()))
+
+       
+   def visualize(self,query):
+        lat, lng = self.gmaps.address_to_latlng(query)
+        self.boba['Coordinates'] = [Point(lng,lat)]
+        updated = self.get_gdf()
+        display(updated.to_json())
+ 
+   def start_chat(self):
        f = open(self.log_file_name, 'w')
        greeting = 'Hello. Where do you want to go today?'
        f.write("Bot: " + greeting + '\n')
        print greeting 
-       cmd = 'espeak ' +  ' '.join(str(k) for k in greeting.split())
-       print cmd
-       os.system(cmd)      
        query = str(raw_input())
        f.write("user: " +  query)
        print "Okay I think you said ", query
+       # Check number of places
+       #num_places = get_numplaces(query)
        time_to_travel = self.get_distance(query)
        print "I think it will take you: ", time_to_travel 
+       gmap = gmplot.GoogleMapPlotter.from_geocode(query)
+       gmap.draw("map.html")
+       print "I have put a map for you in map.html"
 
-    def get_distance(self, query):
+
+   def get_distance(self, query):
        now = datetime.datetime.now()
        directions_result = self.gmaps.directions("Pittsburgh", query, mode='transit', departure_time=now)
        try:
@@ -41,8 +62,10 @@ class DialogAgent(object):
             return s['duration']['text']
        except IndexError:
             print "Hmm. I dont know the answer to that. Sorry"
-            sys.exit()
-           
+            print "I am easily confused. Can you be more specific"
+            query = str(raw_input())
+            return self.get_distance(query)           
+
 DA = DialogAgent()
 #DA.print_logfilename()
 DA.start_chat()
